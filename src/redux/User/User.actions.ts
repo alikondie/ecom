fbbba8bf8ce8934bd29cfef5c6d260c8f809efe8
Taki <takieyamani@gmail.com>
2021-01-08 13:33:57 +0100

    strong typed user action

--- src/redux/User/User.actions.ts
+++ src/redux/User/User.actions.ts
@@ -1,5 +1,6 @@
+import { TAction } from '../../types';
 import * as constants from '../Constants';
-export const setCurrentUser = (user: any) => ({
+export const setCurrentUser = (user: any): TAction => ({
   type: constants.SET_CURRENT_USER,
   payload: user,
 });
